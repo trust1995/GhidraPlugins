ed4333ca4a3ff3d957a6350b53525fdbeca60faf
Or Cyngiser <or.cyngiser@gmail.com>
2019-08-25 18:07:41 +0300

    Support jump to jtex r3 without nop.

--- repair_thunks.py
+++ repair_thunks.py
@@ -8,22 +8,27 @@
 tjex_r3_instr = [24,7,-64,70]
 
 def instr_gen():
-	instr = getFirstInstruction()
-	yield instr
-	while getInstructionAfter(instr):
-		instr = getInstructionAfter(instr)
-		yield instr
-	raise StopIteration()
+    instr = getFirstInstruction()
+    yield instr
+    while getInstructionAfter(instr):
+        instr = getInstructionAfter(instr)
+        yield instr
     
 q = ghidra.app.script.GhidraScript
 for instruction in instr_gen():
-	try:
-		if instruction.getMnemonicString() == u'tjl' and list(q.getBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),currentAddress.getNewAddress(int(str(instruction).split(' ')[-1],16)), 4)) == tjex_r3_instr:
-			print 'Instruction fixup at : {}'.format(instruction.getAddress())
-			q.removeInstructionAt(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
-			q.setBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),instruction.getAddress(),tjex_r3_instr)
-			q.disassemble(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
-	except ghidra.program.model.mem.MemoryAccessException, e:
-		continue
+    try:
+        if instruction.getMnemonicString() == u'tjl':
+            if list(q.getBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),currentAddress.getNewAddress(int(str(instruction).split(' ')[-1],16)), 4)) == tjex_r3_instr:
+                print 'Instruction fixup at : {}'.format(instruction.getAddress())
+                q.removeInstructionAt(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
+                q.setBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),instruction.getAddress(),tjex_r3_instr)
+                q.disassemble(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
+            elif list(q.getBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),currentAddress.getNewAddress(int(str(instruction).split(' ')[-1],16)), 2)) == tjex_r3_instr[:2]:
+                print 'Instruction fixup no nop at : {}'.format(instruction.getAddress())
+                q.removeInstructionAt(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
+                q.setBytes(ghidra.program.flatapi.FlatProgramAPI(currentProgram),instruction.getAddress(),tjex_r3_instr[:2])
+                q.disassemble(ghidra.program.flatapi.FlatProgramAPI(currentProgram), instruction.getAddress())
+    except ghidra.program.model.mem.MemoryAccessException, e:
+        continue
 
 
